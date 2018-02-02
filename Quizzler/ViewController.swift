bab7b9859afe6b931a05009e8e093b2d4bed8de0
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2018-02-02 23:48:27 +0700

    init

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Riza Ega Satyabudhi on 2/02/2018.
+
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Place your instance variables here
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    @IBAction func answerPressed(_ sender: AnyObject) {
+  
+    }
+    
+    
+    func updateUI() {
+      
+    }
+    
+
+    func nextQuestion() {
+        
+    }
+    
+    
+    func checkAnswer() {
+        
+    }
+    
+    
+    func startOver() {
+       
+    }
+    
+
+    
+}
